50abc42849ec83d09e0055988ebe7962ccb4814f
Sergey <rogozhinis@mail.ru>
2022-12-30 11:56:20 +0500

    HomeWork3

---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача21. Напишите программу,которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты точки A:");
+Console.Write("x1=");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1=");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1=");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B:");
+Console.Write("x2=");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2=");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2=");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double AB = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+Console.WriteLine($"Расстояние между точками: {Math.Round(AB,2)}"); 
---
+++ task23/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача №23.Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int N3 = 0;
+int n = 0;
+
+if(N == 0)
+{
+    Console.WriteLine(0);
+}
+
+for (n = 1; n <= Math.Abs(N); n++)
+{
+    N3 = n * n * n;
+    if (N<0)
+    {
+        N3=N3 * -1;
+    }
+    Console.WriteLine(N3);
+}
